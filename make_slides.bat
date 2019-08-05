aac542814e28745e4e67f2beedfef7302cf97ad5
Doosik Kim <doosik@etri.re.kr>
2019-08-05 22:20:26 +0900

    Update README. Update script.

---
+++ make_slides.bat
@@ -0,0 +1,28 @@
+
+
+jupyter nbconvert --to=slides --output-dir=./slides/ README.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 10_python/11_Introduction.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 10_python/12_pip.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 10_python/13_Anaconda.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 10_python/14_Jupyter_Notebook.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 10_python/15_A_Byte_of_Python.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 10_python/16_Numpy.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 10_python/17_Matplotlib.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 10_python/18_Scikit_learn.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 20_opencv/21_Introduction.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 20_opencv/22_Image_Processing.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 20_opencv/23_Drawing_Functions.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 20_opencv/24_Feature_Detection.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 20_opencv/25_Video_Processing.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 20_opencv/26_Machine_Learning.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 30_deep_learning/31_Bayesian_Inference.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 30_deep_learning/32_Linear_Classification.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 30_deep_learning/33_MLP.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 30_deep_learning/34_CNN.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 30_deep_learning/35_Supervised_Learning.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 30_deep_learning/36_Applications.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 40_tensorflow/41_Introduction.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 40_tensorflow/42_Basics.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 40_tensorflow/43_Keras.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 40_tensorflow/44_Models.ipynb
+jupyter nbconvert --to=slides --output-dir=./slides/ 40_tensorflow/45_TensorBoard.ipynb
